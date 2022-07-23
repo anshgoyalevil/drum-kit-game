a0a7d7b75fa63a794579bb5219dea31448cc69d8
Ansh <anshgoyal1704@gmail.com>
2022-07-23 12:23:20 +0530

    Initial commit of all the game files

---
+++ index.js
@@ -0,0 +1,51 @@
+function call(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            console.log("wrong key pressed");
+
+    }
+
+}
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var ih = this.innerHTML;
+        call(ih);
+
+    });
+
+}
+
+document.addEventListener("keydown", function(e){
+    call(e.key);
+});
